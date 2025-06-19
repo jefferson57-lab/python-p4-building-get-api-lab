d612210b85e5937b901619fc9c75ae612cf06fb9
Jefferson <jeff.wafula1@student.moringaschool.com>
2025-06-19 11:52:09 +0300

    after completion

--- server/app.py
+++ server/app.py
@@ -18,21 +18,27 @@ db.init_app(app)
 def index():
     return '<h1>Bakery GET API</h1>'
 
-@app.route('/bakeries')
+@app.route('/bakeries', methods=['GET'])
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    return jsonify([bakery.to_dict() for bakery in bakeries])
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=['GET'])
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    if not bakery:
+        return make_response(jsonify({"error": "Bakery not found"}), 404)
 
+    return jsonify(bakery.to_dict()), 200
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price).all()
+    return jsonify([baked_good.to_dict() for baked_good in baked_goods])
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return jsonify(baked_good.to_dict()) if baked_good else ('', 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
